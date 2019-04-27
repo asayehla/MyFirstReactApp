24b6cd96358dd9489608e98736bc1b8e06c63bcb
Åsa Palholmen <asa@palholmen.se>
2019-04-27 13:22:54 +0200

    connect to starwar-api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    swCharacters: []
+  }
+  componentDidMount() {
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+    axios.get(proxyUrl + 'https://swapi.co/api/people').then(response => {
+      const data = response.data.results
+      this.setState({ swCharacters: data });
+      console.log(data);
+    })
+
+  }
+  render() {
+    let swCharactersList = this.state.swCharacters.map(swCharacter => {
+      return (<li key={swCharacter.name}>{swCharacter.name}</li>)
+    })
+
+    return (
+      <>
+        <ul>
+          {swCharactersList}
+        </ul>
+      </>
+    )
+  }
 }
 
 export default App;
